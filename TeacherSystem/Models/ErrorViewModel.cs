40dc3370d23f968648c87d7ca32ad3cde3c2c805
Djurinschi Andrei <djurinschi.andreiai@gmail.com>
2024-12-29 22:07:07 +0200

    first commit

---
+++ TeacherSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TeacherSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
